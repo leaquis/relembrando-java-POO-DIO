4c21e3a87c4e91154905e703a0e2529ffe5b8829
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-11-05 01:14:04 -0300

    Inicio atividade MSN

---
+++ Pilares de POO/exemplosPilaresDePOO/src/MSN/MSNMessenger.java
@@ -0,0 +1,19 @@
+package MSN;
+
+public class MSNMessenger {
+	public void enviarMensagem() {
+		System.out.println("Enviando mensagem");
+	}
+	
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem");
+	}
+	
+	public void validarConectadoInternet() {
+		System.out.println("Validando se esta conectado a internet");
+	}
+	
+	public void salvarHistoricoMensagem() {
+		System.out.println("Salvando histórico de mensagem");
+	}
+}
