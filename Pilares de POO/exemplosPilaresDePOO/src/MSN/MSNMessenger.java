aaba29c19a26d1355bf3b2aafeb97c88538b53e8
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-11-09 03:45:09 -0300

    encapsulamento

--- Pilares de POO/exemplosPilaresDePOO/src/MSN/MSNMessenger.java
+++ Pilares de POO/exemplosPilaresDePOO/src/MSN/MSNMessenger.java
@@ -2,18 +2,22 @@ package MSN;
 
 public class MSNMessenger {
 	public void enviarMensagem() {
+		validarConectadoInternet();
+		
 		System.out.println("Enviando mensagem");
+		
+		salvarHistoricoMensagem();
 	}
 	
 	public void receberMensagem() {
 		System.out.println("Recebendo mensagem");
 	}
 	
-	public void validarConectadoInternet() {
+	private void validarConectadoInternet() {
 		System.out.println("Validando se esta conectado a internet");
 	}
 	
-	public void salvarHistoricoMensagem() {
+	private void salvarHistoricoMensagem() {
 		System.out.println("Salvando histórico de mensagem");
 	}
 }
